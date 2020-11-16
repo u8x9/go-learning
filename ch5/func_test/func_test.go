a18225aae94e0b22e53d0d7632c550587c1b99f0
u8x9 <u8x@eclipso.eu>
2020-11-17 00:36:16 +0800

    可变参数和defer

--- ch5/func_test/func_test.go
+++ ch5/func_test/func_test.go
@@ -33,3 +33,25 @@ func TestReturnMultiValues(t *testing.T) {
 	a := timeSpend(slowFunc)(123)
 	t.Log(a)
 }
+
+func sum(ops ...int) int {
+	s := 0
+	for _, i := range ops {
+		s += i
+	}
+	return s
+}
+func TestVarParam(t *testing.T) {
+	s := sum(12, 34, 56, 78, 90)
+	t.Log(s)
+}
+
+func clear(t *testing.T) {
+	t.Log("clear resources")
+}
+func TestDefer(t *testing.T) {
+	defer clear(t)
+	t.Log("Start")
+	panic("err")
+	t.Log("End")
+}
